5751ac57ed8f39ea49045dec2826a654f9cf69c3
Ian Kerich <iankerich254@gmail.com>
2024-02-11 17:32:36 +0300

    Update base_model.py documentation

--- models/base_model.py
+++ models/base_model.py
@@ -8,18 +8,23 @@ Defines all common attributes/methods for other classes.
 
 
 class BaseModel:
+    """The class defines all common attributes/methods for other classes"""
     def __init__(self):
+        """This defines public instance attributes"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
+        """This prints [<class name>] (<self.id>) <self.__dict__>"""
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """Updates the public instance attribute updated_at"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__"""
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = type(self).__name__
         obj_dict['created_at'] = self.created_at.isoformat()
